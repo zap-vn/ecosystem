00c7dcb3fb8bdf4a0f6caddf1ff5fe5ab4df1930
Tommy <tom6573637618@gmail.com>
2026-04-14 13:52:43 +0700

    Auto-mocking: Fix reflection for ValueTuples and IEnumerables to prevent ArgumentNullException in MediatR handlers

--- src/Services/Ecosystem/CRM/ZAP.Ecosystem.API.CRM/MockRepositoryProxy.cs
+++ src/Services/Ecosystem/CRM/ZAP.Ecosystem.API.CRM/MockRepositoryProxy.cs
@@ -16,41 +16,58 @@ namespace ZAP.Ecosystem.API.CRM
             if (returnType.IsGenericType && returnType.GetGenericTypeDefinition() == typeof(Task<>))
             {
                 var taskType = returnType.GetGenericArguments()[0];
-                object? defaultResult = null;
-
-                // Handle PagedResult<T> gracefully to avoid crashes in handlers
-                if (taskType.IsGenericType && taskType.GetGenericTypeDefinition().Name == "PagedResult`1")
-                {
-                    var entityType = taskType.GetGenericArguments()[0];
-                    var listType = typeof(List<>).MakeGenericType(entityType);
-                    var emptyList = Activator.CreateInstance(listType);
-
-                    defaultResult = Activator.CreateInstance(taskType);
-                    var itemsProp = taskType.GetProperty("items", BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
-                    if (itemsProp != null)
-                    {
-                        itemsProp.SetValue(defaultResult, emptyList);
-                    }
-                    var totalProp = taskType.GetProperty("total_record", BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
-                    if (totalProp != null)
-                    {
-                        totalProp.SetValue(defaultResult, 0);
-                    }
-                }
-                else if (taskType.IsClass && taskType != typeof(string))
-                {
-                    try { defaultResult = Activator.CreateInstance(taskType); } catch { }
-                }
-                else if (taskType.IsValueType)
-                {
-                    defaultResult = Activator.CreateInstance(taskType);
-                }
+                object? defaultResult = CreateSafeDefault(taskType);
 
                 var method = typeof(Task).GetMethod(nameof(Task.FromResult))!.MakeGenericMethod(taskType);
                 return method.Invoke(null, new[] { defaultResult });
             }
 
-            return returnType.IsValueType ? Activator.CreateInstance(returnType) : null;
+            if (returnType == typeof(Task)) 
+                return Task.CompletedTask;
+
+            return CreateSafeDefault(returnType);
+        }
+
+        private object? CreateSafeDefault(Type type)
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition().Name == "PagedResult`1")
+            {
+                var entityType = type.GetGenericArguments()[0];
+                var listType = typeof(List<>).MakeGenericType(entityType);
+                var emptyList = Activator.CreateInstance(listType);
+
+                var defaultResult = Activator.CreateInstance(type);
+                var itemsProp = type.GetProperty("items", BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+                if (itemsProp != null) itemsProp.SetValue(defaultResult, emptyList);
+                var totalProp = type.GetProperty("total_record", BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+                if (totalProp != null) totalProp.SetValue(defaultResult, 0);
+                return defaultResult;
+            }
+            if (type.IsValueType && type.Name.StartsWith("ValueTuple"))
+            {
+                var tupleArgs = type.GetGenericArguments();
+                var argInstances = new object?[tupleArgs.Length];
+                for (int i = 0; i < tupleArgs.Length; i++)
+                {
+                    argInstances[i] = CreateSafeDefault(tupleArgs[i]);
+                }
+                return Activator.CreateInstance(type, argInstances);
+            }
+            if (type.IsGenericType && (type.GetGenericTypeDefinition() == typeof(IEnumerable<>) || type.GetGenericTypeDefinition() == typeof(IReadOnlyList<>)))
+            {
+                var entityType = type.GetGenericArguments()[0];
+                var listType = typeof(List<>).MakeGenericType(entityType);
+                return Activator.CreateInstance(listType);
+            }
+            if (type.IsClass && type != typeof(string))
+            {
+                try { return Activator.CreateInstance(type); } catch { return null; }
+            }
+            if (type.IsValueType)
+            {
+                return Activator.CreateInstance(type);
+            }
+            return null;
         }
 
         public static T Create<T>()
